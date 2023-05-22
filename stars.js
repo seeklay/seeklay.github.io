c601a77db75022ba9733e88efd74861fe28f8266
seeklay <57865404+seeklay@users.noreply.github.com>
2023-05-23 00:39:19 +0700

    up

--- stars.js
+++ stars.js
@@ -20,11 +20,6 @@ makestars = () => {
             },
             "polygon": {
               "nb_sides": 52
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
             }
           },
           "opacity": {
